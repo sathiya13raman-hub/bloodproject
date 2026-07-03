2ae3047dc074d9d17c5fa96ab9cd99a9850c0297
sathiya13raman-hub <sathiya13.raman@gmail.com>
2026-07-03 13:34:56 +0530

    deployment error solving

--- backend/blood_backend/settings.py
+++ backend/blood_backend/settings.py
@@ -109,7 +109,7 @@ CORS_ALLOWED_ORIGINS = [
     'https://bloodzy.netlify.app',
 ]
 
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_METHODS = list(default_methods)
 CORS_ALLOW_HEADERS = list(default_headers) + [
     'Content-Type',
